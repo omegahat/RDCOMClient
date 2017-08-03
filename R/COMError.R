68d6cbd7a86ab7542932c5ec6a19141e70e4737a
Duncan Temple Lang <duncan@r-project.org>
2017-08-03 13:20:15 +0100

    writeErrors function

--- R/COMError.R
+++ R/COMError.R
@@ -6,3 +6,13 @@ function(msg, status, class = "COMError")
   class(e) = c(class, class(e))
   stop(e)
 }
+
+
+writeErrors =
+function(val = logical())
+{
+    if(length(val))
+        .Call("RDCOM_setWriteErrors", as.logical(val))
+    else
+        .Call("RDCOM_getWriteErrors")
+}
