444a39d3da1087ba841b2aed711907da38720e8e
Duncan Temple Lang <duncan@r-project.org>
2022-06-05 16:35:44 -0700

    added definition of PROBLEM and related macros that were hidden by default in the R 4.2 headers

--- src/RCOMObject.h
+++ src/RCOMObject.h
@@ -291,3 +291,17 @@ class RCOMSObject : public RCOMObject
   SEXP getCallExpression(SEXP func, DISPPARAMS *parms, DISPID id, WORD method, SEXP *ptr);  
 #endif
 };
+
+
+
+
+#ifndef PROBLEM
+
+#define R_PROBLEM_BUFSIZE	4096
+#define PROBLEM			{char R_problem_buf[R_PROBLEM_BUFSIZE];(snprintf)(R_problem_buf, R_PROBLEM_BUFSIZE,
+#define ERROR			),Rf_error(R_problem_buf);}
+#define WARNING(x)		),Rf_warning(R_problem_buf);}
+#define WARN			WARNING(NULL)
+
+#endif /* PROBLEM not defined?*/
+
