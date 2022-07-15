e1d919886741a9c1e929a4b593fea0c4d9ecb912
Duncan Temple Lang <duncan@r-project.org>
2022-07-15 13:26:50 -0700

    documentation for R CMD check + some exports, useDynLib()

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,6 @@
-.First.lib <-
-function(lib, pkg) {
- library.dynam("RDCOMClient", pkg, lib)
- .COMInit()
-}
-
 .onLoad <-
 function(lib, pkg) {
- library(methods)
- library.dynam("RDCOMClient", pkg, lib)
+ #library.dynam("RDCOMClient", pkg, lib)
  .COMInit()
 }
 
